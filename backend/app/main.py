a68a51c7459d40094cac0219936408874848150b
Julian Mendez <julianlx@hotmail.com>
2024-05-11 22:22:22 -0500

    feat: adding jwt token

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,34 +1,21 @@
 import bcrypt
-from typing_extensions import Annotated
 from typing import Union
-from fastapi import FastAPI, HTTPException, Depends
+from pydantic import BaseModel
+from typing_extensions import Annotated
+from datetime import datetime, timedelta
 from app.models import User, UserLogging
 from db.supabase import create_supabase_client
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, HTTPException, Depends
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from pydantic import BaseModel
-
-fake_users_db = {
-    "johndoe": {
-        "username": "johndoe",
-        "full_name": "John Doe",
-        "email": "johndoe@example.com",
-        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
-        "disabled": False,
-    }
-}
-
-class User(BaseModel):
-    username: str
-    email: Union[str, None] = None
-    full_name: Union[str, None] = None
-    disabled: Union[bool, None] = None
+from jose import jwt, JWTError
 
 app = FastAPI()
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 # Initialize supabase client
 supabase = create_supabase_client()
+
+oauth2_scheme = OAuth2PasswordBearer("/token")
 
 app.add_middleware(
     CORSMiddleware,
@@ -38,51 +25,122 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-def user_exists(key: str = "email", value: str = None):
+SECRET_KEY = "27bc57a3b8df8d18c14f3d59278857e5ddc2bfd812b89ef444ce39a56e39895c"
+ALGORITHM = "HS256"
+
+
+def user_exists(key: str = "user_name", value: str = None):
     user = supabase.from_("users").select("*").eq(key, value).execute()
     return len(user.data) > 0
 
+
 def verify_password(plain_password, hashed_password):
-    return bcrypt.checkpw(plain_password.encode('utf-8'), hashed_password)
+    return bcrypt.checkpw(
+        plain_password.encode("utf-8"), hashed_password.encode("utf-8")
+    )
 
-# def authenticate_user(db, username, password):
-    
 
-@app.get("/items/")
-async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
-    return {"token": token}
+def get_user_data(username: str):
+    user = (
+        supabase.from_("users")
+        .select("name", "user_name", "password")
+        .eq("user_name", username)
+        .execute()
+    )
+    return user
 
-#test endpoint
-@app.get("/users/me")
-def user(token: str = Depends(oauth2_scheme)):
-    print(token)
-    return "Im a user"
 
+def authenticate_user(username: str, password: str):
+    if not user_exists(value=username):
+        raise HTTPException(
+            status_code=401,
+            detail="User does not exist ",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    user_data = get_user_data(username)
+
+    if not verify_password(password, user_data.data[0]["password"]):
+        raise HTTPException(
+            status_code=401,
+            detail="Password error",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+
+def create_token(data: dict, time_expire: Union[datetime, None] = None):
+    data_copy = data.copy()
+    if time_expire is None:
+        expires = datetime.now() + timedelta(minutes=15)
+    else:
+        expires = datetime.now() + time_expire
+    data_copy.update({"exp": expires})
+    token_jwt = jwt.encode(data_copy, SECRET_KEY, algorithm=ALGORITHM)
+    return token_jwt
+
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    try:
+        token_decode = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM], options={'verify_exp':False})
+        username = token_decode.get("sub")
+        if username == None:
+            raise HTTPException(
+                status_code=401,
+                detail="Not username",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+    except JWTError:
+        print(JWTError)
+        raise HTTPException(
+            status_code=401,
+            detail="JWT error decoding",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    user_data = get_user_data(username)
+    if not user_data:
+        raise HTTPException(
+            status_code=401,
+            detail="User not exist",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return token_decode
+
+
+# Create token
 @app.post("/token")
 def login(form_data: OAuth2PasswordRequestForm = Depends()):
-    print(form_data.username, form_data.password)
-    return {
-        "access_token": "my_token",
-        "token_type": "bearer"
-    }
+    authenticate_user(form_data.username, form_data.password)
+    access_token_expires = timedelta(minutes=30)
+    access_token_jwt = create_token({"sub": form_data.username}, access_token_expires)
+
+    return {"access_token": access_token_jwt, "token_type": "bearer"}
+
 
 # Create a new user
 @app.post("/user")
 def create_user(user: User):
     try:
         # Convert email to lowercase
-        user_email = user.email.lower()
+        user_name = user.username.lower()
         # Hash password
-        hased_password = bcrypt.hashpw(user.password, bcrypt.gensalt())
+        hased_password = bcrypt.hashpw(user.password.encode("utf-8"), bcrypt.gensalt())
 
         # Check if user already exists
-        if user_exists(value=user_email):
+        if user_exists(value=user_name):
             return {"message": "User already exists"}
 
         # Add user to users table
-        user = supabase.from_("users")\
-            .insert({"name": user.name, "email": user_email, "password": hased_password})\
+        user = (
+            supabase.from_("users")
+            .insert(
+                {
+                    "name": user.name,
+                    "user_name": user_name,
+                    "password": hased_password.decode("utf-8"),
+                }
+            )
             .execute()
+        )
 
         # Check if user was added
         if user:
@@ -93,42 +151,27 @@ def create_user(user: User):
         print("Error: ", e)
         return {"message": "User creation failed"}
 
+
 # Retrieve a user
 @app.get("/user")
-def get_user(user_id: Union[str, None] = None):
-    try:
-        if user_id:
-            user = supabase.from_("users")\
-                .select("id", "name", "email")\
-                .eq("id", user_id)\
-                .execute()
-
-            if user:
-                return user
-        else:
-            users = supabase.from_("users")\
-                .select("id", "email", "name")\
-                .execute()
-            if users:
-                return users
-    except Exception as e:
-        print(f"Error: {e}")
-        return {"message": "User not found"}
+def get_user(user: User = Depends(get_current_user)):
+    return user
 
 
-#logging
+# logging
 @app.post("/login")
-async def login(user: UserLogging):
+async def login(token: Annotated[str, Depends(oauth2_scheme)]):
     try:
         # Convert email to lowercase
         user_email = user.email.lower()
 
         # Check if user already exists
         if user_exists(value=user_email):
-            user_from_db = supabase.from_("users").select("*").eq('email', user_email).execute()
-            print(user.password)
-            if user_from_db.data[0]["password"] == user.password:            
-               return {"message": "User Authenticated"}
+            user_from_db = (
+                supabase.from_("users").select("*").eq("email", user_email).execute()
+            )
+            if user_from_db.data[0]["password"] == user.password:
+                return {"message": "User Authenticated"}
             else:
                 raise HTTPException(status_code=404, detail="email or password error")
 
--- backend/app/models.py
+++ backend/app/models.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel
 
 class User(BaseModel):
     name: str
-    email: str
+    username: str
     password: str
 
 class UserLogging(BaseModel):
